0bc2382ceafde7d6fbb2eeb5e93bcc6d623da6bd
usrnameuser <150504703+usrnameuser@users.noreply.github.com>
2024-07-01 13:33:25 +0300

    Update NamingConvention.java

--- src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
+++ src/main/java/school/mjc/stage0/module3/task2/NamingConvention.java
@@ -3,7 +3,7 @@ package school.mjc.stage0.module3.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variable = 10;
-        int variable_one = 1;
+        int variableOne = 1;
         int variableTwo = 2;
         int variable_three = 3;
         final int VARIABLE_FOUR = 4;
